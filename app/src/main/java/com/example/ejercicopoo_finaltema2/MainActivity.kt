6d903ffada5809c18c4bb33de6d54c41057eee2c
YuboLin-GitH <yubo.lin@educa.jcyl.es>
2025-10-21 10:46:43 +0200

    diseño echo

---
+++ app/src/main/java/com/example/ejercicopoo_finaltema2/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.ejercicopoo_finaltema2
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.ejercicopoo_finaltema2.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var mibinding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}

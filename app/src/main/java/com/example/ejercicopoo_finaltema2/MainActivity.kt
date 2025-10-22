63b378b28c814f535b01aa4ff9bc889808fa39da
yubo <yubo.lin@educa.jcyl.es>
2025-10-22 21:49:21 +0200

    echo spinner

--- app/src/main/java/com/example/ejercicopoo_finaltema2/MainActivity.kt
+++ app/src/main/java/com/example/ejercicopoo_finaltema2/MainActivity.kt
@@ -1,24 +1,30 @@
 package com.example.ejercicopoo_finaltema2
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.example.ejercicopoo_finaltema2.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var mibinding: ActivityMainBinding
+    private lateinit var mibinding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        mibinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mibinding.root)
+
+
+        val tipos = listOf("UsuarioNormal", "Administrador")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, tipos)
+        mibinding.spinner.adapter = adapter
+
+
     }
+
+
+
+
 }
---
+++ app/src/main/java/com/example/ejercicopoo_finaltema2/Usuario.kt
@@ -0,0 +1,12 @@
+package com.example.ejercicopoo_finaltema2
+
+import java.util.*
+
+open class Usuario(
+    var login: String,
+    var password: String,
+    fechaNacParam: Date = Date(),
+    var email: String = ""
+)
+
+
